5df632f9e4e645491495fd999e983d2684a234b7
Jannik Zschiesche <jz@becklyn.com>
2019-11-26 11:39:56 +0100

    Finalize run implementation

--- bin/run.php
+++ bin/run.php
@@ -2,23 +2,44 @@
 
 use Becklyn\TranslationsExtractor\Extractor\TranslationExtractor;
 use Becklyn\TranslationsExtractor\Extractor\Twig\MockExtension;
+use Composer\Autoload\ClassLoader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/** @var ClassLoader|null $loader */
+$loader = null;
 
-var_dump(dirname(__DIR__) . "/vendor/autoload.php");
-var_dump(dirname(__DIR__, 3) . "/autoload.php");
-if (is_file($autoloader = dirname(__DIR__) . "/vendor/autoload.php"))
+if (is_file($autoloaderPath = dirname(__DIR__) . "/vendor/autoload.php"))
 {
-    require_once $autoloader;
+    $loader = include $autoloaderPath;
 }
-else if (is_file($autoloader = dirname(__DIR__, 3) . "/autoload.php"))
+else if (is_file($autoloaderPath = dirname(__DIR__, 3) . "/autoload.php"))
 {
-    require_once $autoloader;
+    $loader = include $autoloaderPath;
 }
+
+if (null === $loader)
+{
+    throw new RuntimeException("Could not determine library composer autoload structure");
+}
+
+// include project's autoloader
+if (is_file($projectAutoloader = dirname(__DIR__, 6) . "/vendor/composer/autoload_psr4.php"))
+{
+    $projectVendors = include $projectAutoloader;
+
+    foreach ($projectVendors as $prefix => $paths)
+    {
+        $loader->addPsr4($prefix, $paths);
+    }
+}
+
+// register autoloader for annotations
+AnnotationRegistry::registerLoader([$loader, "loadClass"]);
 
 $commandName = "extract-translations";
 $version = "0.0.1";
@@ -26,9 +47,9 @@ $version = "0.0.1";
 (new Application($commandName, $version))
     ->register($commandName)
     ->addArgument("directories", InputArgument::IS_ARRAY | InputArgument::REQUIRED, "The directories to extract the translations from")
-    ->addOption("mock-functions", null, InputOption::VALUE_IS_ARRAY, "Twig functions to shim", [])
-    ->addOption("mock-filters", null, InputOption::IS_ARRAY, "Twig filters to shim", [])
-    ->addOption("mock-tests", null, InputOption::IS_ARRAY, "Twig tests to shim", [])
+    ->addOption("mock-functions", null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, "Twig functions to shim", [])
+    ->addOption("mock-filters", null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, "Twig filters to shim", [])
+    ->addOption("mock-tests", null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, "Twig tests to shim", [])
     ->setCode(
         function (InputInterface $input, OutputInterface $output)
         {
@@ -39,7 +60,7 @@ $version = "0.0.1";
             );
 
             $extractor = new TranslationExtractor($mockExtension);
-            $output->writeln(json_encode($extractor->extract($input->getArgument("directory"))));
+            $output->writeln(json_encode($extractor->extract($input->getArgument("directories"))));
             return 0;
         }
     )
